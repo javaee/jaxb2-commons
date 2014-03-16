b9d727c1777d9edc63f2c2ebaff7eee4a771761b
Aleksei Valikov <lexi@java.net>
2014-03-16 09:31:40 +0000

    commons-lang-plugin: JAXB2_COMMONS-34: Migrate to commons-lang3.
    svn path=/commons-lang-plugin/trunk/; revision=323

--- commons-lang-plugin/src/main/java/com/sun/tools/xjc/addon/commons_lang/XjcCommonsLangPlugin.java
+++ commons-lang-plugin/src/main/java/com/sun/tools/xjc/addon/commons_lang/XjcCommonsLangPlugin.java
@@ -15,10 +15,10 @@
  */
 package com.sun.tools.xjc.addon.commons_lang;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.xml.sax.ErrorHandler;
 
 import com.sun.codemodel.JCodeModel;
